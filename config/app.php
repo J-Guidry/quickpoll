9407d9f066f68467d1654107ea8389dfe00dc9e2
Guidry <jh_guidry@outlook.com>
2018-06-19 17:12:07 -0500

    initial deploy

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://boiling-mesa-39797.herokuapp.com'),
 
     /*
     |--------------------------------------------------------------------------
