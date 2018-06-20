c64999fb527c58073273a797344eed19c94d1d18
Guidry <jh_guidry@outlook.com>
2018-06-19 21:02:39 -0500

    initial deploy

--- app/Http/Controllers/PollController.php
+++ app/Http/Controllers/PollController.php
@@ -3,11 +3,84 @@
 namespace quickpoll\Http\Controllers;
 
 use Illuminate\Http\Request;
+use quickpoll\Poll;
+use quickpoll\PollOption;
 
 class PollController extends Controller
 {
     //
     public function index(){
         return view("index");
+    }
+
+    public function store(Request $request){
+        $input = $request->all();
+
+        $input['poll_name'] = filter_var($input['poll_name'], FILTER_SANITIZE_STRING);
+        $input['poll_option'] = filter_var_array($input['poll_option']);
+        $request->replace($input);  
+    
+        $request->validate([
+            'poll_name' => 'required|string',
+            'poll_option' => 'required',
+        ]);
+        
+        $poll = new Poll;
+        $poll->poll_name = $request->poll_name;
+    
+        $poll->save();
+        $id = $poll->getAttribute('id');
+    
+        $option = $request->poll_option;
+    
+        $options = [];
+    
+        for($i = 0; $i < count($option); $i++){
+            $poption =  new PollOption;
+            $options[] = $poption->fill([
+                'option_name' => $option[$i],
+                'poll_id' => $id
+            ]);
+            $poption->save();
+        }
+        //dd(response($id));
+        return $id;
+        //return redirect("/poll/$id");
+       // return response()->json(['id' => $id]);
+    }
+
+    public function show($id){
+        $options = Poll::find($id)->options;
+        $poll_name = Poll::find($id)->poll_name;
+        
+        $option_names = [];
+        foreach($options as $option){
+            $option_names[] = $option->getAttributes();
+        }
+    
+        return view('poll',['title' => $poll_name, 'options' => $option_names, 'id' => $id]);
+    }
+
+    public function update($id,Request $request){
+        $input = $request->all();
+        //dd($input);
+        $input['poll_name'] = filter_var($input['votes'], FILTER_SANITIZE_STRING);
+        $request->replace($input);  
+    
+        $request->validate([
+            'votes' => 'required|string',
+        ]);
+    
+        $option_voted_for = $request->votes;
+    
+        $find_vote = PollOption::where([        
+            ['poll_id', '=', $id],
+            ['option_name','=', $option_voted_for]
+            ])->firstOrFail();
+        $vote = $find_vote;
+        $vote->vote_count += 1;
+        $vote->save();
+    
+        return redirect("/results/$id");
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -24,77 +24,11 @@ use quickpoll\Vote;
 
 Route::get('/', "PollController@index");
 
-Route::post('/polls', function(Request $request) {
-    $input = $request->all();
+Route::post('/polls', "PollController@store");
 
-    $input['poll_name'] = filter_var($input['poll_name'], FILTER_SANITIZE_STRING);
-    $input['poll_option'] = filter_var_array($input['poll_option']);
-    $request->replace($input);  
+Route::get('/poll/{id}', "PollController@show");
 
-    $request->validate([
-        'poll_name' => 'required|string',
-        'poll_option' => 'required',
-    ]);
-    
-    $poll = new Poll;
-    $poll->poll_name = $request->poll_name;
-
-    $poll->save();
-    $id = $poll->getAttribute('id');
-
-    $option = $request->poll_option;
-
-    $options = [];
-
-    for($i = 0; $i < count($option); $i++){
-        $poption =  new PollOption;
-        $options[] = $poption->fill([
-            'option_name' => $option[$i],
-            'poll_id' => $id
-        ]);
-        $poption->save();
-    }
-    //dd(response($id));
-    return $id;
-    //return redirect("/poll/$id");
-   // return response()->json(['id' => $id]);
-
-});
-
-Route::get('/poll/{id}', function($id){
-    $options = Poll::find($id)->options;
-    $poll_name = Poll::find($id)->poll_name;
-    
-    $option_names = [];
-    foreach($options as $option){
-        $option_names[] = $option->getAttributes();
-    }
-
-    return view('poll',['title' => $poll_name, 'options' => $option_names, 'id' => $id]);
-});
-
-Route::put("/vote/{id}", function($id,Request $request){
-    $input = $request->all();
-    //dd($input);
-    $input['poll_name'] = filter_var($input['votes'], FILTER_SANITIZE_STRING);
-    $request->replace($input);  
-
-    $request->validate([
-        'votes' => 'required|string',
-    ]);
-
-    $option_voted_for = $request->votes;
-
-    $find_vote = PollOption::where([        
-        ['poll_id', '=', $id],
-        ['option_name','=', $option_voted_for]
-        ])->firstOrFail();
-    $vote = $find_vote;
-    $vote->vote_count += 1;
-    $vote->save();
-
-    return redirect("/results/$id");
-});
+Route::put("/vote/{id}", "PollController@update");
 
 Route::get('/results/{id}', function($id){
     $find_results = Poll::find($id)->options;
@@ -112,7 +46,6 @@ Route::get('/results/{id}', function($id){
     $poll_title = Poll::find($id)->getAttribute('poll_name');
 
     return view('results',['title' => $poll_title, 'options'=> $options_array, 'votes' => $votes_array, 'id' => $id]);
-
 });
 
 Route::get('/search_results', function(Request $request){
@@ -126,7 +59,6 @@ Route::get('/search_results', function(Request $request){
     $name_id = [];
     foreach($polls as $poll){
         $name_id[] = ['id' => $poll['id'], 'name' => $poll['poll_name']];
-
     };
 
     $results = $name_id;
